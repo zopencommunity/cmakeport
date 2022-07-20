cbeb91da55b9a4524be92f50d55e0cad523dc780
Igor Todorovski <itodorov@ca.ibm.com>
2022-07-19 21:21:25 -0400

    Remove polling of utils.git

--- cicd.groovy
+++ cicd.groovy
@@ -10,6 +10,6 @@ node('linux')
         }
 
         stage('Build') {
-                build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'cmakeport'), string(name: 'DESCRIPTION', 'CMake is used to control the software compilation process using simple platform and compiler independent configuration files, and generate native makefiles.' )]
+                build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'cmakeport'), string(name: 'DESCRIPTION', value: 'CMake is used to control the software compilation process using simple platform and compiler independent configuration files, and generate native makefiles.' )]
         }
 }
