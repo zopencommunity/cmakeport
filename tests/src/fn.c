c4c917f2c89c1076899d4e29d80bcf73e18b8d2d
Mike Fulton <mikefultonpersonal@gmail.com>
2022-12-19 18:09:50 -0500

    use ninja

---
+++ tests/src/fn.c
@@ -0,0 +1,7 @@
+#include "hw.h"
+#include <stdio.h>
+
+int hw() {
+  return printf("Hello world\n");
+}
+
--- tests/src/hw.c
+++ tests/src/hw.c
@@ -1,9 +1,5 @@
-#include <stdio.h>
 #include "hw.h"
 
-int hw() {
-  return printf("Hello world\n");
-}
 int main(int argc, char* argv[]) {
   return hw();
 }
