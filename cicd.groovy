6a2f8a258f21fa83bf1fe8801ca04b4de61e5ce8
Igor Todorovski <39890068+IgorTodorovskiIBM@users.noreply.github.com>
2023-09-28 15:54:45 -0400

    Run on v2r4 only due to 0C4

--- cicd.groovy
+++ cicd.groovy
@@ -10,6 +10,6 @@ node('linux')
         }
 
         stage('Build') {
-                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/cmakeport.git'), string(name: 'PORT_DESCRIPTION', value: 'CMake is used to control the software compilation process using simple platform and compiler independent configuration files, and generate native makefiles.' )]
+                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/cmakeport.git'), string(name: 'PORT_DESCRIPTION', value: 'CMake is used to control the software compilation process using simple platform and compiler independent configuration files, and generate native makefiles.' ),  string(name: 'NODE_LABEL', value: "v2r4")]
         }
 }
