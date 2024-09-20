34af8a8bf90c49582c4618c3dc521c01ef090bb0
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:20:22 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -6,10 +6,10 @@ node('linux')
                         branches: [[name: '*/main']],
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [],
-                        userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/cmakeport.git']]])
+                        userRemoteConfigs: [[url: 'https://github.com/zopencommunity/cmakeport.git']]])
         }
 
         stage('Build') {
-                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/cmakeport.git'), string(name: 'PORT_DESCRIPTION', value: 'CMake is used to control the software compilation process using simple platform and compiler independent configuration files, and generate native makefiles.' )]
+                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/cmakeport.git'), string(name: 'PORT_DESCRIPTION', value: 'CMake is used to control the software compilation process using simple platform and compiler independent configuration files, and generate native makefiles.' )]
         }
 }
